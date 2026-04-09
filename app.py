905ca1f0e935278893fa90daa500edc55b2a5e2a
70878778 <85946867+70878778@users.noreply.github.com>
2026-04-09 16:55:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-pip install plotly
+! pip install plotly
 import plotly.express as px
 pip install geopandas
 import geopandas as gpd
